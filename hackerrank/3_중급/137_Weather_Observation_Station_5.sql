e0851c980d015e793771e3fb8cffdaf8b271372b
muahseol-hub <muahseol@gmail.com>
2026-01-19 20:49:47 +0900

    [HackerRank] 중급 - Weather Observation Station 5 (#137)

---
+++ hackerrank/3_중급/137_Weather_Observation_Station_5.sql
@@ -0,0 +1,7 @@
+-- Weather Observation Station 5
+-- HackerRank 중급 (⭐⭐⭐)
+-- 문제 링크: https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+-- 작성자: test9
+-- 작성일: 2026. 01. 19. 20:49:44
+
+LIMIT 1);
